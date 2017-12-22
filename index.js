3647e8a63f44724be46903a01905bcf4300ab81f
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-22 17:48:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 // Code your solution in this file!
 
-// function logDriverNames(drivers) {
-//   for (const el of drivers) {
-//     console.log(el.name)
-//   }
-// }
-//
-// function logDriversByHometown(drivers, town) {
-//   for (const el of drivers) {
-//     if(el.hometown === town) {
-//       console.log(el.name)
-//     }
-//   }
-// }
-//
-// function driversByRevenue(drivers){
-//   let new_array = [...drivers]
-//
-//   new_array.sort(function (a, b){
-//      return a.revenue - b.revenue
-//   })
-//
-//   return new_array
-// }
+function logDriverNames(drivers) {
+  for (const el of drivers) {
+    console.log(el.name)
+  }
+}
+
+function logDriversByHometown(drivers, town) {
+  for (const el of drivers) {
+    if(el.hometown === town) {
+      console.log(el.name)
+    }
+  }
+}
+
+function driversByRevenue(drivers){
+  let new_array = [...drivers]
+
+  new_array.sort(function (a, b){
+     return a.revenue - b.revenue
+  })
+
+  return new_array
+}
 
 function driversByName(drivers) {
   let new_array = [...drivers]
