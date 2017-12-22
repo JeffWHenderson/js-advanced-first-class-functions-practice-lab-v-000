90233bac76320bf62259a2bad0463e59ea850ba6
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-22 17:34:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ function logDriversByHometown(drivers, town) {
 
 function driversByRevenue(drivers){
   let new_array = []
-  new_array = drivers['revenue'].sort()
+  new_array = drivers.sort(a, b) {
+    a.revenue - b.revenue
+  }
   // for (const el of drivers){
   //   console.log(el.revenue)
   // }
